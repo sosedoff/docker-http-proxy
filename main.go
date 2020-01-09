2d888006d4dbe3c6fb997aa936e3aaa0fb4d95c3
Dan Sosedoff <dan.sosedoff@gmail.com>
2020-01-09 12:22:53 -0600

    Config update; add options to redirect from root

--- main.go
+++ main.go
@@ -1,33 +1,65 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "net/http/httputil"
-    "net/url"
-    "os"
+	"errors"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
 )
 
-func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
-
-    target := os.Getenv("TARGET_URL")
-    if target == "" {
-        log.Fatal("TARGET_URL is not provided")
-    }
-
-    targetURL, err := url.Parse(target)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    proxy := httputil.NewSingleHostReverseProxy(targetURL)
-
-    if err := http.ListenAndServe("0.0.0.0:"+port, proxy); err != nil {
-        log.Fatal(err)
-    }
+type config struct {
+	port         string
+	target       *url.URL
+	redirectRoot string
 }
 
+func initConfig() (*config, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	target := os.Getenv("TARGET_URL")
+	if target == "" {
+		return nil, errors.New("TARGET_URL is not provided")
+	}
+
+	targetURL, err := url.Parse(target)
+	if err != nil {
+		return nil, err
+	}
+
+	c := config{
+		port:         port,
+		target:       targetURL,
+		redirectRoot: os.Getenv("TARGET_REDIRECT_ROOT"),
+	}
+
+	return &c, nil
+}
+
+func main() {
+	cfg, err := initConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(cfg.target)
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		// Redirect the home page to some other path
+		if req.URL.Path == "/" && cfg.redirectRoot != "" {
+			http.Redirect(rw, req, cfg.redirectRoot, 302)
+			return
+		}
+
+		// Pass request to the backend
+		proxy.ServeHTTP(rw, req)
+	})
+
+	if err := http.ListenAndServe("0.0.0.0:"+cfg.port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
